c8f49f31d65cab5f41695c0da3a49fda1082c8da
Muff1ne <236550389+Muff1ne@users.noreply.github.com>
2025-10-09 20:26:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-  
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
 
- 
-  }
-
-*/
 
 function myFunction() {
   const nav = document.getElementById("myTopnav");
   const icon = document.querySelector(".icon i");
 
   nav.classList.toggle("responsive");
-  icon.classList.toggle("open");  // toggle special style
+  icon.classList.toggle("open"); 
 }
